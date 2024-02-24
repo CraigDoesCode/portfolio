e52b199d9ea7e2e21b84757e96253f1c80b0bf74
Craig Norford <craig.a.norford@gmail.com>
2024-02-24 23:40:25 +0000

    test: simple test for banner

---
+++ src/components/home/banner.spec.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { Banner } from "./banner";
+import "@testing-library/jest-dom";
+
+describe("Banner", () => {
+  it("renders banenr", () => {
+    render(<Banner />);
+
+    const heading = screen.getByRole("heading", {
+      name: /Hi, I'm Craig 👋/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
