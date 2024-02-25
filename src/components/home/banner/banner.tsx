bea4e3af180e98aaf8337a77608efc4bbf3a9dbe
Craig Norford <craig.a.norford@gmail.com>
2024-02-25 20:35:50 +0000

    style: animate banner background

--- src/components/home/banner/banner.tsx
+++ src/components/home/banner/banner.tsx
@@ -2,7 +2,7 @@ import styles from "./Banner.module.scss";
 export const Banner = () => {
   return (
     <section className={`${styles.banner} ${styles.container}`}>
-      <h1 className={styles.text}>FullStack | Software Engineer 👋</h1>
+      <h1 className={styles.text}>FullStack | Engineer | Educator | EdTech</h1>
     </section>
   );
 };
